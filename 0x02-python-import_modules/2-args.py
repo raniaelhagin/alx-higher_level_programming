350e88c74696975c6e85180d37d98d7bcb57bfc3
raniaelhagin <raniahamada0308@gmail.com>
2023-08-11 23:20:12 +0200

    dynamic script fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,15 +3,15 @@
 import sys
 
 if __name__ == "__main__":
-    argvalues = sys.argv
-    argv_len = len(argvalues) - 1
+    argvalues = sys.argv[1:]
+    argv_len = len(argvalues)
 
     if argv_len == 0:
         print("0 arguments.")
     elif argv_len == 1:
         print("1 argument:")
-        print("1: ", argvalues[1])
+        print("1: ", argvalues[0])
     else:
         print("{:d} arguments:".format(argv_len))
         for i in range(1, argv_len + 1):
-            print("{:d}: {:s}".format(i, argvalues[i]))
+            print("{:d}: {:s}".format(i, argvalues[i - 1]))

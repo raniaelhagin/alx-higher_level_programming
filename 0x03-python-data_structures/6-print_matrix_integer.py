56c41c3a8ab3cfb41ff5d3d778b2372387522f6a
raniaelhagin <raniahamada0308@gmail.com>
2023-08-15 03:11:12 +0200

    print a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 1:
         print("")
-    if matrix:
+    if len(matrix) > 1:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if j != len(matrix[i]) - 1:

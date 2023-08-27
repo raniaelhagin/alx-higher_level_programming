c1551689f6dd69ef0e1c328511b649517a7fe56b
raniaelagin <raniaamada0308@gmail.com>
2023-08-27 22:27:34 +0200

    run inline python code

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: pointer to the linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	if (list->n == 0)
+		return (0);
+
+	else
+		return (1);
+}

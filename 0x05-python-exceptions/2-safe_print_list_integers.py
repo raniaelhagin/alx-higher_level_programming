4c4d5945398dd45c4fa553d0da01b3477769745d
raniaelhagin <raniahamada0308@gmail.com>
2023-08-28 13:17:01 +0300

    debug integer division

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     pr_items = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             pr_items += 1
-        except IndexError:
+        except (ValueError, TypeError):
             pass
     print("")
     return (pr_items)

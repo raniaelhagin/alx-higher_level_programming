b6507c63fc664575bbcdc7f53f0722866272263b
raniaelhagin <raniahamada0308@gmail.com>
2023-10-03 00:42:14 +0200

    updates

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
-"""Unittest for max integer in a list"""
-
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMaxInt(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
     """ Defines unittest for max int"""
 
     def test_ordered_list(self):
@@ -21,13 +20,13 @@ class TestMaxInt(unittest.TestCase):
         list_float = [3.7, 4.4, 1.2, 2.6]
         self.assertEqual(max_integer(list_float), 4.4)
 
-    def test_float_list(self):
-        list_float = [3, 4.4, 1, 2.6]
-        self.assertEqual(max_integer(list_float), 4.4)
+    def test_int_float_list(self):
+        list_int_float = [3, 4.4, 1, 2.6]
+        self.assertEqual(max_integer(list_int_float), 4.4)
 
-    def test_float_list(self):
-        list_float = [-3, -4.4, 1, 2.6]
-        self.assertEqual(max_integer(list_float), 2.6)
+    def test_int_float_neg_list(self):
+        list_int_float_neg = [-3, -4.4, 1, 2.6]
+        self.assertEqual(max_integer(list_int_float_neg), 2.6)
 
     def test_unit_list(self):
         list_unit = [89]
